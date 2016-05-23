70185b6131c585d90c8eba06adceab46cc8d7d32
Lionel Guérin <lionelguerin.mail@gmail.com>
2016-05-23 09:31:40 +0200

    max on home page OK etape 6

--- src/Ens/LionelBundle/Controller/JobController.php
+++ src/Ens/LionelBundle/Controller/JobController.php
@@ -26,7 +26,7 @@ class JobController extends Controller
 
         foreach($categories as $category)
         {
-            $category->setActiveJobs($em->getRepository('EnsLionelBundle:Job')->getActiveJobs($category->getId()));
+            $category->setActiveJobs($em->getRepository('EnsLionelBundle:Job')->getActiveJobs($category->getId(), $this->container->getParameter('max_jobs_on_homepage')));
         }
 
         return $this->render('job/index.html.twig', array(
