4627b6901883ea4327d8c8cabc36bd2a5aee9896
Lionel Guérin <lionelguerin.mail@gmail.com>
2016-05-23 09:49:02 +0200

    début etape 7

--- src/Ens/LionelBundle/Controller/JobController.php
+++ src/Ens/LionelBundle/Controller/JobController.php
@@ -27,6 +27,7 @@ class JobController extends Controller
         foreach($categories as $category)
         {
             $category->setActiveJobs($em->getRepository('EnsLionelBundle:Job')->getActiveJobs($category->getId(), $this->container->getParameter('max_jobs_on_homepage')));
+            $category->setMoreJobs($em->getRepository('EnsLionelBundle:Job')->countActiveJobs($category->getId()) - $this->container->getParameter('max_jobs_on_homepage'));
         }
 
         return $this->render('job/index.html.twig', array(
