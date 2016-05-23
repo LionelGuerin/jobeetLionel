a2f3dd09f7c7a558592bab28421c0f0bf9ee034f
Lionel Guérin <lionelguerin.mail@gmail.com>
2016-05-23 09:56:38 +0200

    page category OK

--- src/Ens/LionelBundle/Controller/CategoryController.php
+++ src/Ens/LionelBundle/Controller/CategoryController.php
@@ -9,5 +9,20 @@ use Ens\LionelBundle\Entity\Category;
  */
 class CategoryController extends Controller
 {
+    public function showAction($slug)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
 
+        $category = $em->getRepository('EnsLionelBundle:Category')->findOneBySlug($slug);
+
+        if (!$category) {
+            throw $this->createNotFoundException('Unable to find Category entity.');
+        }
+
+        $category->setActiveJobs($em->getRepository('EnsLionelBundle:Job')->getActiveJobs($category->getId()));
+
+        return $this->render('category/show.html.twig', array(
+            'category' => $category,
+        ));
+    }
 }
