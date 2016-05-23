aa36d47dbb98b9431f9d5d663ca159871db94661
Lionel Guérin <lionelguerin.mail@gmail.com>
2016-05-23 09:53:54 +0200

    etape 7 suite

---
+++ src/Ens/LionelBundle/Controller/CategoryController.php
@@ -0,0 +1,13 @@
+<?php
+namespace Ens\LionelBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Ens\LionelBundle\Entity\Category;
+
+/**
+ * Category controller.
+ *
+ */
+class CategoryController extends Controller
+{
+
+}
